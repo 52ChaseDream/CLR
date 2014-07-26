b4383a56167b9e44fdd46a2fb650af1f4de64a96
wyb <haitianwyb@sina.com>
2014-07-26 16:11:52 +0800

    添加了一个自定义控件

---
+++ CLR.CommonUserControl/SecurityControl.Designer.cs
@@ -0,0 +1,155 @@
+﻿using CLR.Security;
+namespace CLR.CommonUserControl
+{
+    partial class SecurityControl
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboSecurityMode = new System.Windows.Forms.ComboBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSource.BackColor = System.Drawing.SystemColors.Window;
+            this.txtSource.Location = new System.Drawing.Point(0, 0);
+            this.txtSource.Margin = new System.Windows.Forms.Padding(0);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(350, 25);
+            this.txtSource.TabIndex = 0;
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDestination.BackColor = System.Drawing.SystemColors.Window;
+            this.txtDestination.Location = new System.Drawing.Point(0, 55);
+            this.txtDestination.Margin = new System.Windows.Forms.Padding(0);
+            this.txtDestination.Multiline = true;
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(350, 25);
+            this.txtDestination.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "类型：";
+            // 
+            // cboSecurityMode
+            // 
+            this.cboSecurityMode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.cboSecurityMode.FormattingEnabled = true;
+            this.cboSecurityMode.Items.AddRange(new object[] {
+            "Base64",
+            "DES",
+            "MD5",
+            "RSACryption",
+            "TripleDES"});
+            this.cboSecurityMode.Location = new System.Drawing.Point(55, 6);
+            this.cboSecurityMode.Name = "cboSecurityMode";
+            this.cboSecurityMode.Size = new System.Drawing.Size(121, 20);
+            this.cboSecurityMode.TabIndex = 11;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnDecrypt.Location = new System.Drawing.Point(187, 4);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 24);
+            this.btnDecrypt.TabIndex = 9;
+            this.btnDecrypt.Text = "解  密";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnEncrypt.Location = new System.Drawing.Point(273, 4);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 24);
+            this.btnEncrypt.TabIndex = 10;
+            this.btnEncrypt.Text = "加  密";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += (sender, e) => { this.txtDestination.Text = this.Security(this.cboSecurityMode.SelectedItem.ToString(), SecurityMode.ENCRYPT, this.txtSource.Text); };
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.btnDecrypt);
+            this.panel1.Controls.Add(this.cboSecurityMode);
+            this.panel1.Controls.Add(this.btnEncrypt);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 30);
+            this.panel1.TabIndex = 13;
+            // 
+            // SecurityControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "SecurityControl";
+            this.Size = new System.Drawing.Size(350, 80);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.SizeChanged += (sender, e) => { this.txtDestination.Height = this.txtSource.Height = this.panel1.Location.Y; };
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboSecurityMode;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ CLR.CommonUserControl/SecurityControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Forms;
+using CLR.Security;
+using System;
+
+namespace CLR.CommonUserControl
+{
+    public partial class SecurityControl : UserControl
+    {
+        private SecurityUtility _securityUtility;
+
+        public SecurityControl()
+        {
+            InitializeComponent();
+            this.cboSecurityMode.SelectedIndex = 0;
+        }
+
+        private void btnDecrypt_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                this.txtDestination.Text = this.Security(this.cboSecurityMode.SelectedItem.ToString(), SecurityMode.DECRYPT, this.txtSource.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("该功能暂时还未实现！！！");
+            }
+
+        }
+
+        protected string Security(string securityControlMode, SecurityMode securityMode, string source)
+        {
+            switch (securityControlMode)
+            {
+                case "Base64":
+                    _securityUtility = new Base64Helper();
+                    break;
+                case "DES":
+                    _securityUtility = new DESHelper();
+                    break;
+                case "MD5":
+                    _securityUtility = new MD5Helper();
+                    break;
+                case "RSACryption":
+                    _securityUtility = new RSACryptionHelper();
+                    break;
+                case "TripleDES":
+                    _securityUtility = new TripleDESHelper();
+                    break;
+                default:
+                    break;
+            }
+            return _securityUtility.SecurityUtilityInvoke(securityMode).Invoke(source);
+        }
+    }
+}
